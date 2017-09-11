fb1f7b0d6bfd44e64dc76978d0674697953c90d4
Paulet <paula.cgs@hotmail.com>
2017-09-11 18:06:30 -0300

    auxs

--- obdd.asm
+++ obdd.asm
@@ -25,7 +25,7 @@ extern dictionary_add_entry
 ;OFFSETS nodo:
 %define offset_var_ID 0
 %define offset_node_ID 4
-%define offset_red_count 8
+%define offset_ref_count 8
 %define offset_high_obdd 12
 %define offset_low_obdd 16
 
@@ -100,8 +100,71 @@ obdd_mgr_mk_node:
 	pop rbp
 	ret
 
+	
+; void obdd_node_destroy(obdd_node* node){
+	; if(node->ref_count == 0){
+		; if(node->high_obdd != NULL){
+			; obdd_node* to_remove = node->high_obdd;
+			; node->high_obdd	= NULL;
+			; to_remove->ref_count--;
+			; obdd_node_destroy(to_remove);
+		; }
+		; if(node->low_obdd != NULL){
+			; obdd_node* to_remove = node->low_obdd;
+			; node->low_obdd	= NULL;
+			; to_remove->ref_count--;
+			; obdd_node_destroy(to_remove);
+		; }
+		; node->var_ID	= 0;
+		; node->node_ID	= 0;
+		; free(node);
+	; }
+; }	
+	
 global obdd_node_destroy
 obdd_node_destroy:
+;RDI es puntero a node
+.stackframe
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r8
+	push r9
+.begin
+	; if(node->ref_count == 0){
+	cmp [rdi + offset_ref_count], 0
+	jne .end
+	; if(node->high_obdd != NULL){
+	cmp [rdi + offset_high_obdd], NULL
+	je .compareLowObdd
+	; obdd_node* to_remove = node->high_obdd;
+	mov r8, [rdi + offset_high_obdd]
+	; node->high_obdd	= NULL;
+	mov byte [rdi + offset_high_obdd], NULL
+	; to_remove->ref_count--;
+	sub [r8 + offset_ref_count], 1
+	; obdd_node_destroy(to_remove);
+	mov rdi, r8
+	call obdd_node_destroy
+	; if(node->low_obdd != NULL){
+.compareLowObdd
+	cmp [r8 + offset_low_obdd], NULL
+	je .terminar
+	mov r9, [rdi + offset_low_obdd]
+	mov byte [r8 + offset_low_obdd], NULL
+	sub [r9 + offset_ref_count], 1
+	mov rdi, r9
+	call obdd_node_destroy
+.terminar
+	mov byte [r9 + offset_var_ID], 0
+	mov byte [r9 + offset_node_ID], 0
+	mov rdi, r8
+	call free
+.end
+	pop r9
+	pop r8
+	pop rbx
+	pop rbp
 ret
 
 ; /** implementar en ASM
@@ -115,14 +178,17 @@ ret
 
 global obdd_create
 obdd_create:
+; TODO
 ret
 
 global obdd_destroy
 obdd_destroy:
+; TODO
 ret
 
 global obdd_node_apply
 obdd_node_apply:
+; TODO
 ret
 
 ; /** implementar en ASM
@@ -191,19 +257,96 @@ is_tautology:
 
 global is_sat
 is_sat:
+; TODO
 ret
 
 
 ; AUXILIARES
 
 global str_len
+; RDI es char* a
 str_len:
-ret
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+
+	xor rbx, rbx
+	mov r12, rdi
+	
+.ciclo
+	cmp r12d, NULL
+	je .end
+	add ebx, 1
+	lea r12, [r12 + 4]
+	jmp .ciclo
+.end
+	mov eax, ebx	
+	pop r12
+	pop rbx
+	pop rbp
+	ret
 
 global str_copy
 str_copy:
-ret
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+	
+	mov rbx, rdi
+	xor rax, rax
+	call str_len
+	mov ebx, eax
+	mov rdi, rax
+	sar rdi, 2
+	call malloc
+	
+.ciclo	
+	cmp ebx, 0
+	je .end
+	mov r12, [rbx]
+	mov [rax], r12
+	lea rbx, [rbx + 4]
+	lea rax, [rax + 4]
+	sub ebx, 1
+	jmp .ciclo
+	
+.end
+	pop r12
+	pop rbx
+	pop rbp
+	ret
 
 global str_cmp
+; RDI char* a
+; RSI char* b
 str_cmp:
-ret
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r12
+.ciclo	
+	mov rbx, [rdi]
+	mov r12, [rsi]
+	cmp ebx, r12d
+	jl .returnLess
+	jg .returnGreater
+	cmp ebx, 0
+	je .returnEquals
+	lea rbx, [rbx + 4]
+	lea r12, [r12 + 4]
+	jmp .ciclo
+.returnEquals
+	mov rax, 0
+	jmp .end
+.returnLess
+	mov rax, 1
+	jmp .end
+.returnGreater
+	mov rax,-1
+.end
+	pop r12
+	pop rbx
+	pop rbp
+	ret
